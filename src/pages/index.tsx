76725645f8995364284e96facac1a6fa8ad98b9b
yyycc <892838772@qq.com>
2023-01-01 09:44:30 +0800

    add plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import {StaticImage} from 'gatsby-plugin-image'
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby Tutorial.</p>
+            <StaticImage
+                height={200}
+                width={200}
+                alt="Kitty with glasses"
+                src="../images/favicon.jpeg"
+            />
         </Layout>
     )
 }
