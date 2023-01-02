c684363ce369f8b83a9ba4576f4417b7df957d72
yyycc <892838772@qq.com>
2023-01-02 18:54:30 +0800

    Working with Images in MDX

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,14 +11,41 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-image", "gatsby-plugin-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "blog",
-      "path": `${__dirname}/blog`
+  plugins: [
+    "gatsby-plugin-image",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "blog",
+        "path": `${__dirname}/blog`
+      },
+      __key: "pages"
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+  ]
 };
 
 export default config;
