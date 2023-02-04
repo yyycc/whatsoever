15eb969fd1c3470afd1ae05d5aff8ea4b2981560
yyycc <892838772@qq.com>
2023-02-04 19:27:37 +0800

    Add Ts Blog

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -3,4 +3,4 @@ import './src/style.scss'
 import './src/styles/index.scss'
 import 'prismjs/themes/prism-solarizedlight.css'
 
-console.log("%cWelcome to my blog ~ ~", "color:#8a4baf")
+console.log("%cWelcome to my blog ~ ~", "color:#8a4baf;background-color:#fcfaff;padding: 4px 8px;border-radius: 5px;")
