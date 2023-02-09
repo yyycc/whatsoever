014d72a8d005cf2a504a71783196a12724a9e49d
yyycc <892838772@qq.com>
2023-02-09 14:46:18 +0800

    Add TypeScript Router

---
+++ src/pages/blog/front-end/TypeScript.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from "../../../components/layout"
+import FrontEnd from "../../../components/front-end/front-end"
+
+const TypeScript = () => {
+  return (
+    <Layout className="type-script">
+      <FrontEnd tag="TypeScript" title="TypeScript"/>
+    </Layout>
+  )
+}
+
+export default TypeScript
