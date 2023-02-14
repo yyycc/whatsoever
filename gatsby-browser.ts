314af5cf388aa49eb6d52a1cf2573d2321df2356
yyycc <892838772@qq.com>
2023-02-14 20:41:24 +0800

    Update Console

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -4,3 +4,10 @@ import './src/styles/index.scss'
 import 'prismjs/themes/prism-solarizedlight.css'
 
 console.log("%cWelcome to my blog ~ ~", "color:#8a4baf;background-color:#fcfaff;padding: 4px 8px;border-radius: 5px;")
+console.log('%c' +
+  '           _           _                                 \n' +
+  ' __      _| |__   __ _| |_ ___  ___   _____   _____ _ __ \n' +
+  ' \\ \\ /\\ / / \'_ \\ / _` | __/ __|/ _ \\ / _ \\ \\ / / _ \\ \'__|\n' +
+  '  \\ V  V /| | | | (_| | |_\\__ \\ (_) |  __/\\ V /  __/ |   \n' +
+  '   \\_/\\_/ |_| |_|\\__,_|\\__|___/\\___/ \\___| \\_/ \\___|_|   \n' +
+  '                                                         \n', "color: #8a4baf")
