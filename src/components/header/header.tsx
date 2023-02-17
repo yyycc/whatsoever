220e307533524aff7da737ed00dcafcc0cacff22
yyycc <892838772@qq.com>
2023-02-17 09:56:28 +0800

    Adjust Mobile

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,9 +2,10 @@ import * as React from "react"
 import './header.scss'
 import { Link } from "gatsby"
 import { Dropdown, Space } from "antd"
-import { DownOutlined } from '@ant-design/icons'
+import { DownOutlined, MenuOutlined } from '@ant-design/icons'
 import { spaceToHyphen } from "../../utils/common"
 import { IFrontmatter } from "../data"
+import type { MenuProps } from 'antd';
 
 const Header = ({ title, blogData }: { title: string, blogData: Array<{ frontmatter: IFrontmatter }> }) => {
   const allFolders: IFrontmatter[] = blogData.map(ele => ({
@@ -21,6 +22,39 @@ const Header = ({ title, blogData }: { title: string, blogData: Array<{ frontmat
     const folders = folder?.split(',') // folder内容支持,分隔，含有$符号的folder不使用tag作为二级目录
     folders?.forEach(fol => (showFolders[fol] || (showFolders[fol] = [])).push(ele))
   })
+  const orderedShow = Object.keys(showFolders).sort((a: string, b: string) => {
+    if (a.indexOf('$') > -1) {
+      return -1
+    }
+    return 1
+  })
+  const multiItems: MenuProps['items'] = []
+  orderedShow.forEach(order => {
+    if (order.indexOf('$') > -1) {
+      const label = order.replace('$', '')
+      multiItems.push({
+        key: label,
+        label: <Link to={`/blog/${spaceToHyphen(label)}`}> {label}</Link>,
+      })
+    } else {
+      const data = showFolders[order]
+      const tags: string[] = [...new Set(data.map((ele: IFrontmatter) => ele.tag))]
+      const children: MenuProps['items'] = []
+      tags.forEach(tag => {
+        children.push({
+          key: tag,
+          label: (
+            <Link to={`/blog/${spaceToHyphen(order)}/${tag}`}>{tag}</Link>
+          )
+        })
+      })
+      multiItems.push({
+        key: order,
+        label: order,
+        children
+      })
+    }
+  })
   return (
     <div className="header">
       <div className="header-content">
@@ -30,12 +64,7 @@ const Header = ({ title, blogData }: { title: string, blogData: Array<{ frontmat
           </Link>
         </div>
         <div className="header-content-nav">
-          {Object.keys(showFolders).sort((a: string, b: string) => {
-            if (a.indexOf('$') > -1) {
-              return -1
-            }
-            return 1
-          }).map(folder => {
+          {orderedShow.map(folder => {
             const data = showFolders[folder]
             const tags: string[] = [...new Set(data.map((ele: IFrontmatter) => ele.tag))]
             const items = tags.map((ele: string) => ({
@@ -47,7 +76,6 @@ const Header = ({ title, blogData }: { title: string, blogData: Array<{ frontmat
               const singleFolder = folder.replace('$', '')
               return <div key={folder} className="header-content-nav__div">
                 <Link to={`/blog/${spaceToHyphen(singleFolder)}`}> {singleFolder}</Link>
-
               </div>
             }
             return (
@@ -59,6 +87,11 @@ const Header = ({ title, blogData }: { title: string, blogData: Array<{ frontmat
               </Dropdown>
             )
           })}
+        </div>
+        <div className="header-content-bar">
+          <Dropdown className="header-content-nav__dropdown" menu={{ items: multiItems }}>
+            <MenuOutlined/>
+          </Dropdown>
         </div>
       </div>
     </div>
