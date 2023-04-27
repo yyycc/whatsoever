ba2f37fe006a31a62e1ca76f69c0db6146f4718b
yyycc <892838772@qq.com>
2023-04-27 10:05:56 +0800

    Update JavaScript

---
+++ src/pages/blog/front-end/JavaScript.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from "../../../components/layout"
+import FrontEnd from "../../../components/front-end/front-end"
+
+const JavaScript = () => {
+  return (
+    <Layout className="java-script">
+      <FrontEnd tag="JavaScript" title="JavaScript"/>
+    </Layout>
+  )
+}
+
+export default JavaScript
