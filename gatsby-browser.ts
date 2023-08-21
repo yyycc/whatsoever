eee39ff5b397ce062b5397b6f76078de00cf9164
yyycc <892838772@qq.com>
2023-08-21 09:41:36 +0800

    add ssr blog

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -11,3 +11,16 @@ console.log('%c' +
   '  \\ V  V /| | | | (_| | |_\\__ \\ (_) |  __/\\ V /  __/ |   \n' +
   '   \\_/\\_/ |_| |_|\\__,_|\\__|___/\\___/ \\___| \\_/ \\___|_|   \n' +
   '                                                         \n', "color: #8a4baf")
+
+const addScript = (url: string) => {
+  const script = document.createElement("script")
+  script.src = url
+  // script.async = true
+  document.head.appendChild(script)
+}
+
+export const onClientEntry = () => {
+  window.onload = () => {
+    addScript("https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML")
+  }
+}
