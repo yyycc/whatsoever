7837c947fe9a20281c1a2cb9eb29d8f6205c61a9
Chen Yinying <chenyinying@patsnap.com>
2024-03-29 16:40:39 +0800

    update about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,7 +13,8 @@ const AboutPage = () => {
         <p>前一阵子整理前些年写的一些博客，想搬到这个站点来，结果发现写得都太粗浅了，让我彻底歇了用陈年文章填充博客的心思。</p>
         <p>我想写的并不是那种如何安装如何启动类型的文章，而是介绍原理，追根究底的文章。</p>
         <p>当然写什么样的文章并不是初心，初心是学。</p>
-        <p></p>
+        <p>自认为懂了是第一层，能说于别人理解是第二层，能写明白是第三层，能写得让人明白是第四层。</p>
+        <p>能写下来的，至少能算会8成吧。</p>
         <div className="about-title">About Me</div>
         {/*<p>First some Q&As:</p>*/}
         <p>Q1: Favorite Food</p>
@@ -24,10 +25,10 @@ const AboutPage = () => {
         <p>A3: Awake by the alarm in the morning, take a look at the clock and then go back to sleep.</p>
         <p>Q4: Why this blog</p>
         <p>
-          A4: It's for learning and that's the only reason. I don't write it for other people to read, although somebody reading this would be very nice,
+          A4: It's for learning and that's the only reason. I don't write it for other people to read, although somebody reading this would be really nice,
           but that's not the reason and motivation. It's a method for me to learn staff better and deeper.
           I will not write something I already know well like how to ,um...oh my god I know nothing well...
-          I write things I don't quite get yet, and during which I learn deeper.
+          I write things I don't quite get yet, and during which I can learn better.
         </p>
       </div>
     </Layout>
